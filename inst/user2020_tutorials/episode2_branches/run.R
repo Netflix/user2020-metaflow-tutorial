f5911968094d3fa3fa0ed1dd6b8649e1edc6af3e
Jason Ge <jge@netflix.com>
2020-08-07 08:08:28 -0700

    set default value for parameters

--- inst/user2020_tutorials/episode2_branches/run.R
+++ inst/user2020_tutorials/episode2_branches/run.R
@@ -6,8 +6,8 @@ source("./scripts/build_model.R")
 library(metaflow)
 
 metaflow("HouseFlow") %>%
-    parameter("lr", help="learning rate", type="float", required=TRUE) %>%
-    parameter("reg", help="regularization parameter", type="float", required=TRUE) %>%
+    parameter("lr", help="learning rate", type="float", default=0.01) %>%
+    parameter("reg", help="regularization parameter", type="float", default=0.01) %>%
     step(step = "start",
          next_step = "pull_house_data") %>%
     step(step = "pull_house_data",
