07a9bec902e83d87da6b5257a76082d040da8826
Jason Ge <jge@netflix.com>
2020-08-07 08:06:20 -0700

    update scripts

--- R/pull_tutorials.R
+++ R/pull_tutorials.R
@@ -1,7 +1,7 @@
 #' Pull the USER 2020 Metaflow tutorials to the current folder
 #' @export
 pull_tutorials <- function(){
-  tutorials_folder <- system.file("user2020_tutorials", package = "user2020.metaflow")
+  tutorials_folder <- system.file("user2020_tutorials", package = "user2020metaflow")
   file.copy(tutorials_folder, ".", recursive=TRUE)
   invisible()
 }
